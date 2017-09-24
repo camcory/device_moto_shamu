LineageOS/android_device_huawei_angler

Add thermal HAL
Bug: 32022261
Test: cts
Change-Id: I00ace3d0dbafd8a7cc39535cfad98624081dd3ba

device.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
     libnfc-nci \
     NfcNci \
     Tag \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl
 
 ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES += \
@@ -318,8 +318,12 @@ PRODUCT_PACKAGES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     power.angler \
-    thermal.angler \
     android.hardware.power@1.0-impl \
+
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    thermal.angler \
+    android.hardware.thermal@1.0-impl
 
 ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES += \

camcory/device_moto_shamu

device.mk
@@ -416,7 +416,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += \
     power.shamu \
-    thermal.shamu
+    thermal.shamu \
+    android.hardware.thermal@1.0-impl
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \